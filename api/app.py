0445bbf3f04113448d406cee5fa27d34b23db342
layx <lay9412206@gmail.com>
2023-07-20 16:25:14 +0800

    update

--- api/app.py
+++ api/app.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, abort
 from urllib.parse import parse_qsl
 
-from api.events.basic import *
-from api.events.service import *
+from events.basic import *
+from events.service import *
 # from line_bot_api import *
 
 # from extensions import db, migrate
